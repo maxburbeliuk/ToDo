16c4b150dfe13c7ae9ee9f6376d4ccccaf4c0514
max <example@gmail.com>
2023-09-23 17:36:31 +0300

    add ISE_PATHS into appRotes

--- src/navigators/AppNavigator/appRoutes.js
+++ src/navigators/AppNavigator/appRoutes.js
@@ -1,9 +1,10 @@
-import { APP_PATHS, SERVICES_PATHS } from '~/__constants__'
+import { APP_PATHS, ISE_PATHS, SERVICES_PATHS } from '~/__constants__'
 import { TaskAll, TaskShow } from '~/pages/App'
 import { Navigate } from 'react-router-dom'
 
 const { TASKS_ALL, TASKS_SHOW } = APP_PATHS
 const { NOT_FOUND } = SERVICES_PATHS
+const { INTERNAL_SERVER_ERROR } = ISE_PATHS
 export const APP_ROUTES = [
   { key: TASKS_ALL, path: TASKS_ALL, element: <TaskAll /> },
   { key: TASKS_SHOW, path: TASKS_SHOW, element: <TaskShow /> },
@@ -11,5 +12,10 @@ export const APP_ROUTES = [
     key: NOT_FOUND,
     path: '*',
     element: <Navigate to={`services${NOT_FOUND}`} />
+  },
+  {
+    key: INTERNAL_SERVER_ERROR,
+    path: '*',
+    element: <Navigate to={`services${INTERNAL_SERVER_ERROR}`} />
   }
 ]
