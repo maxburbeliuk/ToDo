d08e96414c62444e88cfc1df6ff84ffbb1365418
okrdima <okrdima@gmail.com>
2023-11-02 23:22:15 +0200

    Updated TaskSimpleMenu

--- src/domains/Task/components/TaskSimpleMenu/TaskSimpleMenu.js
+++ src/domains/Task/components/TaskSimpleMenu/TaskSimpleMenu.js
@@ -1,14 +1,48 @@
-import { Menu, ActionIcon, Checkbox, Flex } from '@mantine/core'
+import { Menu, ActionIcon, Checkbox } from '@mantine/core'
 import {
   MENU_OPTIONS_SORT_BY,
   MENU_OPTIONS_SORT_TYPE
 } from '~/domains/Task/components/__constants__'
 import { toTitleCase } from '~/helpers'
 import { IconSortDescendingLetters } from '@tabler/icons-react'
+import { useState } from 'react'
 
 const TaskSimpleMenu = () => {
+  const [sortBy, setSortBy] = useState({
+    ...Object.keys(MENU_OPTIONS_SORT_BY).reduce(
+      (acc, key) => ({ ...acc, [key]: false }),
+      {}
+    ),
+    [MENU_OPTIONS_SORT_BY.CREATE]: true
+  })
+  const [sortType, setSortType] = useState({
+    ...Object.keys(MENU_OPTIONS_SORT_TYPE).reduce(
+      (acc, key) => ({ ...acc, [key]: false }),
+      {}
+    ),
+    [MENU_OPTIONS_SORT_TYPE.ASC]: true
+  })
+
+  const onChangeSortBy = (selectedSortBy) => {
+    setSortBy((prev) => {
+      const transformed = Object.keys(prev).reduce(
+        (acc, key) => ({ ...acc, [key]: false }),
+        {}
+      )
+      return { ...transformed, [selectedSortBy]: true }
+    })
+  }
+  const onChangeSortType = (selectedType) => {
+    setSortType((prev) => {
+      const transformed = Object.keys(prev).reduce(
+        (acc, key) => ({ ...acc, [key]: false }),
+        {}
+      )
+      return { ...transformed, [selectedType]: true }
+    })
+  }
   return (
-    <Menu shadow="md" width={200} justify="">
+    <Menu width={200} shadow="md">
       <Menu.Target>
         <ActionIcon variant="filled" aria-label="delete">
           <IconSortDescendingLetters size={18} />
@@ -16,22 +50,39 @@ const TaskSimpleMenu = () => {
       </Menu.Target>
 
       <Menu.Dropdown>
-        <Menu.Label>
-          Sort by
-          {Object.entries(MENU_OPTIONS_SORT_BY).map(([key, value]) => (
-            <Menu.Item fw={500} key={key} value={key}>
-              <Checkbox labelPosition="right" label={toTitleCase(value)} />
-            </Menu.Item>
-          ))}
-        </Menu.Label>
-        <Menu.Label>
-          Sort type
-          {Object.entries(MENU_OPTIONS_SORT_TYPE).map(([key, value]) => (
-            <Menu.Item fw={500} key={key} value={key}>
-              <Checkbox labelPosition="right" label={toTitleCase(value)} />
-            </Menu.Item>
-          ))}
-        </Menu.Label>
+        <Menu.Label>Sort by</Menu.Label>
+        {Object.entries(MENU_OPTIONS_SORT_BY).map(([key, value]) => (
+          <Menu.Item
+            fw={500}
+            key={key}
+            value={key}
+            closeMenuOnClick={false}
+            onClick={() => onChangeSortBy(key)}
+          >
+            <Checkbox
+              labelPosition="right"
+              label={toTitleCase(value)}
+              checked={sortBy[key]}
+            />
+          </Menu.Item>
+        ))}
+        <Menu.Divider />
+        <Menu.Label>Sort type</Menu.Label>
+        {Object.entries(MENU_OPTIONS_SORT_TYPE).map(([key, value]) => (
+          <Menu.Item
+            fw={500}
+            key={key}
+            value={key}
+            closeMenuOnClick={false}
+            onClick={() => onChangeSortType(key)}
+          >
+            <Checkbox
+              labelPosition="right"
+              label={toTitleCase(value)}
+              checked={sortType[key]}
+            />
+          </Menu.Item>
+        ))}
       </Menu.Dropdown>
     </Menu>
   )
