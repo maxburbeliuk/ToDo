ba010293ba5a952474c1bc84e888ed4992b8b545
okrdima <okrdima@gmail.com>
2023-11-11 15:51:59 +0200

    Added advanced props for modal

--- src/domains/Task/components/TaskSimpleView/TaskSimpleView.js
+++ src/domains/Task/components/TaskSimpleView/TaskSimpleView.js
@@ -10,12 +10,13 @@ import {
 import { IconEdit, IconTrashFilled } from '@tabler/icons-react'
 import TaskSimpleForm from '~/domains/Task/components/TaskSimpleForm'
 import { useTaskActions } from '~/domains/Task/hooks'
-import { useDisclosure } from '@mantine/hooks'
+import { useDisclosure, useMediaQuery } from '@mantine/hooks'
 
 const TaskSimpleView = (props) => {
   const { text, description, id, done } = props
 
   const [opened, { open, close }] = useDisclosure(false)
+  const isMobile = useMediaQuery('(max-width: 50em)')
 
   const { handleEditTask, handleDeleteTask, handleDone } = useTaskActions()
 
@@ -28,44 +29,56 @@ const TaskSimpleView = (props) => {
   }
 
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Modal opened={opened} onClose={close} title="Edit task" centered>
+    <>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>{text}</Text>
+          <Badge color={'var(--mantine-color-pink-5)'} variant="light">
+            {computedStatus}
+          </Badge>
+        </Group>
+        <Text size="sm" c="dimmed">
+          {description}
+        </Text>
+        <Group gap="md" justify="space-between" mt="md">
+          <Checkbox
+            checked={done}
+            onChange={() => handleDone(id, done)}
+            label={computedCheckBoxLabel}
+          />
+          <Group gap="md">
+            <ActionIcon variant="filled" aria-label="edit" onClick={open}>
+              <IconEdit size={18} />
+            </ActionIcon>
+            <ActionIcon
+              variant="filled"
+              aria-label="delete"
+              color={'var(--mantine-color-red-8)'}
+              onClick={() => handleDeleteTask(id)}
+            >
+              <IconTrashFilled size={18} />
+            </ActionIcon>
+          </Group>
+        </Group>
+      </Card>
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Edit task"
+        centered
+        fullScreen={isMobile}
+        transitionProps={{
+          transition: 'fade',
+          duration: 300,
+          timingFunction: 'linear'
+        }}
+      >
         <TaskSimpleForm
           onSubmit={handleFormSubmit}
           initialValues={{ text, description }}
         />
       </Modal>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{text}</Text>
-        <Badge color={'var(--mantine-color-pink-5)'} variant="light">
-          {computedStatus}
-        </Badge>
-      </Group>
-      <Text size="sm" c="dimmed">
-        {description}
-      </Text>
-      <Group gap="md" justify="space-between" mt="md">
-        <Checkbox
-          checked={done}
-          onChange={() => handleDone(id, done)}
-          label={computedCheckBoxLabel}
-        />
-        <Group gap="md">
-          <ActionIcon variant="filled" aria-label="edit" onClick={open}>
-            <IconEdit size={18} />
-          </ActionIcon>
-          <ActionIcon
-            variant="filled"
-            aria-label="delete"
-            color={'var(--mantine-color-red-8)'}
-            onClick={() => handleDeleteTask(id)}
-          >
-            <IconTrashFilled size={18} />
-          </ActionIcon>
-        </Group>
-      </Group>
-    </Card>
+    </>
   )
 }
 export default TaskSimpleView
