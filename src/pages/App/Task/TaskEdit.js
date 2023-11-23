c73daa766cfbf5f34a0c1fab352639b02b67695d
okrdima <okrdima@gmail.com>
2023-11-23 22:22:00 +0200

    Added initial values on TaskEdit.js

--- src/pages/App/Task/TaskEdit.js
+++ src/pages/App/Task/TaskEdit.js
@@ -1,24 +1,30 @@
 import TaskSimpleForm from '~/domains/Task/components/TaskSimpleForm'
-import { useDisclosure } from '@mantine/hooks'
 import { useTaskActions } from '~/domains/Task/hooks'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useTaskContext } from '~/domains/Task/context'
+import { APP_PATHS } from '~/__constants__'
 
-const TaskEdit = (props) => {
-  const { text, description, id } = props
-  const [_, { close }] = useDisclosure(false)
+const TaskEdit = () => {
+  const navigate = useNavigate()
+  const { taskId } = useParams()
+
+  const { tasks } = useTaskContext()
+
+  const { text, description, id } =
+    tasks?.find((task) => task?.id === taskId) || {}
 
   const { handleEditTask } = useTaskActions()
   const handleFormSubmit = (taskData) => {
     handleEditTask({ ...taskData, id })
-    close()
+
+    navigate(APP_PATHS.TASKS_ALL)
   }
 
   return (
-    <div onClose={close} title="Edit task">
-      <TaskSimpleForm
-        onSubmit={handleFormSubmit}
-        initialValues={{ text, description }}
-      />
-    </div>
+    <TaskSimpleForm
+      onSubmit={handleFormSubmit}
+      initialValues={{ text, description }}
+    />
   )
 }
 export default TaskEdit
